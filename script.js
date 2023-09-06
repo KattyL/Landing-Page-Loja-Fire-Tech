efc942cd985c11ad3d06bb55d13796955a79431e
KattyL <katturync@gmail.com>
2023-09-05 22:50:34 -0300

    salvando projeto, p.s: barras de scrollagem não se comportam como esperado

--- script.js
+++ script.js
@@ -10,19 +10,61 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-let slideIndex = 0;
+let slideIndex = 1;
 
-function showSlides() {
+function showSlides(n) {
     let slides = document.querySelectorAll(".slide");
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
+
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000); 
 }
 
-showSlides(); 
+function nextSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+showSlides(slideIndex);
+
+document.querySelector(".prev").addEventListener("click", () => {
+    nextSlides(-1);
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+    nextSlides(1);
+});
+
+
+//Esconder ao scrollar
+
+const navTop = document.getElementById("nav-top");
+const navBot = document.getElementById("nav-bot");
+const navMid = document.getElementById("nav-mid");
+
+const elementsToMonitor = [navTop, navBot, navMid];
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener("scroll", () => {
+    const currentScrollPos = window.pageYOffset;
+
+    elementsToMonitor.forEach(element => {
+        if (prevScrollPos > currentScrollPos) {
+ 
+            element.classList.remove("hidden");
+        } else {
+            
+            element.classList.add("hidden");
+        }
+    });
+
+    prevScrollPos = currentScrollPos;
+});
